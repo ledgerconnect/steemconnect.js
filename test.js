370bb731c0a5259f6786d414a808b3ed65c5f178
adcpm <fabien@adcpm.com>
2016-08-16 14:03:43 +0700

    - Commit

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 var steemConnect = require('./lib/steemconnect');
 
-var username = 'USERNAME';
-var password = 'PASSWORD';
+var username = process.env.STEEM_USERNAME;
+var password = process.env.STEEM_PASSWORD;
 
+steemConnect.auth.verify(username, password, ['owner', 'active', 'posting']);
+
+/*
 steemConnect.token.get(username, password, function(err, token){
 	console.log(err, token);
 });
+*/
