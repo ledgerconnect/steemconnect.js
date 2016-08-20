9ed81448fad13b4e94e4c51900ad4d948b4d6588
Kaptain Krayola <nhinrichs@gmail.com>
2016-08-19 21:08:18 -0500

    all errors fixed and signature being returned.  Not sure if it's actually valid though- needs testing

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var tx = {
 	expiration: '2016-08-16T16:16:39',
 	extensions: [],
 	operations: [['vote', {
-		voter: process.env.STEEM_USERNAME,
+		voter: "kaptainkrayola",
 		author: 'steemapp',
 		permlink: 'introducing-steemy-fully-native-ios-android-apps-for-steem',
 		weight: 10000
@@ -19,6 +19,8 @@ var tx = {
 	ref_block_prefix: 3799131183,
 	signatures: []
 };
+
+console.log(tx)
 
 //2052021bf67f98b5b4d25015c7fc8a1b6d6fb9c94e766f1e12ea1c15c43e4c577e5383569325a6dc849a64aa473cafdab8ff30922cc5fa162643a4d13e15fd024c
 
