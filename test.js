f67a9c2fb23448c6d475f1e1851a9c0d5d6b7cb7
adcpm <fabien@adcpm.com>
2016-08-21 18:52:07 +0700

    - Commit

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var tx = {
 	operations: [['vote', {
 		voter: username,
 		author: 'mynameisbrian',
-		permlink: 'uber-under-attack-my-first-hand-experience-with-uber-regulation',
+		permlink: 'steemit-weight-loss-challenge-plus',
 		weight: 10000
 	}]],
 	signatures: []
@@ -18,14 +18,15 @@ var tx = {
 var privKeys = steemConnect.auth.getPrivateKeys(username, password, ['posting']);
 
 steem.api.login(username, password, function() {
+	steem.api.getApiByName('network_broadcast_api', function() {});
 	steem.api.getDynamicGlobalProperties(function(err, result) {
-			tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
-			tx.ref_block_num = result.head_block_number & 0xFFFF;
-			tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
-			var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
-			console.log(signedTransaction);
-			steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
-				console.log(err, result);
-			});
+		tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
+		tx.ref_block_num = result.head_block_number & 0xFFFF;
+		tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
+		var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
+		console.log(signedTransaction);
+		steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
+			console.log(err, result);
+		});
 	});
 });
