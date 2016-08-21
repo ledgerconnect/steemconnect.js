6f4864ecbffceefeb2d42120e1f8f382dfd04aa2
adcpm <fabien@adcpm.com>
2016-08-21 18:51:55 +0700

    - Commit

--- dev.js
+++ dev.js
@@ -1,24 +1,32 @@
-var steem = require('./index');
+var steemConnect = require('./lib/steemconnect'),
+	steem = require('steem'),
+	moment = require('moment');
 
 var username = process.env.STEEM_USERNAME;
 var password = process.env.STEEM_PASSWORD;
 
-steem.api.login(username, password, function(err, result) {
-	console.log(result);
-	var trx = {
-		expiration: '2016-08-20T02:59:51',
-		extensions: [],
-		operations: [['vote', {
-			voter: username,
-			author: 'rogerkver',
-			permlink: 'roger-ver-the-world-s-first-bitcoin-investor-is-now-on-steemit',
-			weight: 10000
-		}]],
-		ref_block_num: 40607,
-		ref_block_prefix: 2394871259,
-		signatures: ['206239640514a1aac6ef29b0fdf7bf1f1457526a77bb4cb35bc82d7f614d271bb54783850b6da824db5cf787d7a7b9b8a1da5fd3d2c2ebf53437906fc93f45d238']
-	};
-	steem.api.broadcastTransaction(trx, function(err, result) {
-		console.log(err, result);
+var tx = {
+	extensions: [],
+	operations: [['vote', {
+		voter: username,
+		author: 'mynameisbrian',
+		permlink: 'steemit-weight-loss-challenge-plus',
+		weight: 10000
+	}]],
+	signatures: []
+};
+var privKeys = steemConnect.auth.getPrivateKeys(username, password, ['posting']);
+
+steem.api.login(username, password, function() {
+	steem.api.getApiByName('network_broadcast_api', function() {});
+	steem.api.getDynamicGlobalProperties(function(err, result) {
+		tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
+		tx.ref_block_num = result.head_block_number & 0xFFFF;
+		tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
+		var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
+		console.log(signedTransaction);
+		steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
+			console.log(err, result);
+		});
 	});
 });
--- test.js
+++ test.js
@@ -5,45 +5,27 @@ var steemConnect = require('./lib/steemconnect'),
 var username = process.env.STEEM_USERNAME;
 var password = process.env.STEEM_PASSWORD;
 
-var privKeys = steemConnect.auth.getPrivateKeys(username, password, ['posting']);
-console.log(privKeys);
-
-steem.api.getContent('rogerkver', 'roger-ver-the-world-s-first-bitcoin-investor-is-now-on-steemit', function(err, result) {
-	//console.log(err, result);
-});
-
 var tx = {
-	//expiration: '2016-08-20T02:59:51',
 	extensions: [],
 	operations: [['vote', {
 		voter: username,
 		author: 'mynameisbrian',
-		permlink: 'the-evolution-and-diversity-of-the-steemit-whale-past-present-and-future',
+		permlink: 'uber-under-attack-my-first-hand-experience-with-uber-regulation',
 		weight: 10000
 	}]],
-	//ref_block_num: 40607, // next block
-	//ref_block_prefix: 2394871259,
 	signatures: []
 };
+var privKeys = steemConnect.auth.getPrivateKeys(username, password, ['posting']);
 
-steem.api.getDynamicGlobalProperties(function(err, result) {
-		tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
-		tx.ref_block_num = result.head_block_number & 0xFFFF;
-		tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
-		var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
-		console.log(signedTransaction);
-		steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
-			console.log(err, result);
-		});
+steem.api.login(username, password, function() {
+	steem.api.getDynamicGlobalProperties(function(err, result) {
+			tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
+			tx.ref_block_num = result.head_block_number & 0xFFFF;
+			tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
+			var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
+			console.log(signedTransaction);
+			steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
+				console.log(err, result);
+			});
+	});
 });
-
-//console.log(new Buffer('004123ee28007e0a8820ba419329705e051c3f7a', 'hex').readUInt32LE(4));
-
-/*
-var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
-
-if (signedTransaction.signatures[0] == '206239640514a1aac6ef29b0fdf7bf1f1457526a77bb4cb35bc82d7f614d271bb54783850b6da824db5cf787d7a7b9b8a1da5fd3d2c2ebf53437906fc93f45d238') {
-	console.log('The signature is correct!');
-}
-*/
-
