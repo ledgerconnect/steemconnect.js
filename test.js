bbadabdf57155f77b8275a3c67e3b8b1cb25a454
adcpm <fabien@adcpm.com>
2016-08-21 15:23:51 +0700

    - Commit

--- test.js
+++ test.js
@@ -13,37 +13,37 @@ steem.api.getContent('rogerkver', 'roger-ver-the-world-s-first-bitcoin-investor-
 });
 
 var tx = {
-	expiration: '2016-08-20T02:59:51',
+	//expiration: '2016-08-20T02:59:51',
 	extensions: [],
 	operations: [['vote', {
 		voter: username,
-		author: 'rogerkver',
-		permlink: 'roger-ver-the-world-s-first-bitcoin-investor-is-now-on-steemit',
+		author: 'mynameisbrian',
+		permlink: 'the-evolution-and-diversity-of-the-steemit-whale-past-present-and-future',
 		weight: 10000
 	}]],
-	ref_block_num: 40607, // next block
-	ref_block_prefix: 2394871259,
+	//ref_block_num: 40607, // next block
+	//ref_block_prefix: 2394871259,
 	signatures: []
 };
 
+steem.api.getDynamicGlobalProperties(function(err, result) {
+		tx.expiration = moment.utc(result.timestamp).add(15, 'second').format().replace('Z', '');
+		tx.ref_block_num = result.head_block_number & 0xFFFF;
+		tx.ref_block_prefix =  new Buffer(result.head_block_id, 'hex').readUInt32LE(4);
+		var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
+		console.log(signedTransaction);
+		steem.api.broadcastTransactionWithCallback(function(cb) {}, signedTransaction, function(err, result) {
+			console.log(err, result);
+		});
+});
+
+//console.log(new Buffer('004123ee28007e0a8820ba419329705e051c3f7a', 'hex').readUInt32LE(4));
+
+/*
 var signedTransaction = steemConnect.auth.signTransaction(tx, privKeys);
-//console.log(signedTransaction);
 
 if (signedTransaction.signatures[0] == '206239640514a1aac6ef29b0fdf7bf1f1457526a77bb4cb35bc82d7f614d271bb54783850b6da824db5cf787d7a7b9b8a1da5fd3d2c2ebf53437906fc93f45d238') {
 	console.log('The signature is correct!');
 }
+*/
 
-steem.api.login(username, password, function() {
-	/*steem.api.broadcastTransaction(signedTransaction, function(err, result) {
-		console.log(err, result);
-	});*/
-});
-
-
-steem.api.getDynamicGlobalProperties(function(err, result) {
-	steem.api.getBlock(result.head_block_number, function(err, result) {
-		console.log(result.timestamp);
-		console.log(moment.utc(result.timestamp).add(15, 'second').format().replace('Z', ''));
-		console.log(result.transactions[0].ref_block_num, result.transactions[0].ref_block_num, result.transactions[0].expiration);
-	});
-});
