0c5fbd1c0f231c8b30178388fa0799fbb7210e63
adcpm <fabien@bonustrack.co>
2017-04-21 20:09:32 +0700

    Init SDK with Demo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# sc2-sdk
+# SteemConnect v2 SDK
+
+# Init SDK
+```
+sc2.init({
+  app: 'busy',
+  callbackURL: 'http://localhost:8000/demo/',
+  accessToken: 'access_token'
+});
+```
+
+# Get user profile
+```
+sc2.me(function (err, res) {
+  console.log(err, res)
+});
+```
+
+# Vote
+```
+sc2.vote(voter, author, permlink, weight, function (err, res) {
+  console.log(err, res)
+});
+```
---
+++ demo/app.js
@@ -0,0 +1,30 @@
+sc2.init({
+  baseURL: 'http://localhost:3000',
+  app: 'busy',
+  callbackURL: 'http://localhost:8000/demo/'
+});
+
+angular.module('app', [])
+  .config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true);
+  }])
+  .controller('Main', function($scope, $location, $http) {
+    $scope.accessToken = $location.search().access_token;
+    $scope.expiresIn = $location.search().expires_in;
+    $scope.loginURL = sc2.getLoginURL();
+
+    if ($scope.accessToken) {
+      sc2.setAccessToken($scope.accessToken);
+      sc2.me(function (err, result) {
+        console.log('/me', err, result);
+        $scope.user = result;
+        $scope.$apply();
+      });
+    }
+
+    this.submit = function() {
+      sc2.vote($scope.user.name, 'siol', 'test', 100, function (err, result) {
+        console.log('/broadcast', err, result);
+      });
+    }
+  });
