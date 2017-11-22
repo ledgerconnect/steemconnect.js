bec6a5ccc529d3159db20f971df16ea56f3191c3
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-11-22 16:39:24 +0100

    Make SteemConnect instance based

--- src/sc2.js
+++ src/sc2.js
@@ -9,48 +9,56 @@ class SDKError extends Error {
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor);
     } else {
-      this.stack = (new Error(message)).stack;
+      this.stack = new Error(message).stack;
     }
   }
 }
 
-const sc2 = {
-  baseURL: 'https://v2.steemconnect.com',
-  app: '',
-  callbackURL: '',
-  scope: [],
+const SteemConnect = () => {
+  this.options = {
+    baseURL: 'https://v2.steemconnect.com',
+    app: '',
+    callbackURL: '',
+    scope: [],
+  };
 };
 
-sc2.init = (params) => {
-  if (params.baseURL) sc2.setBaseURL(params.baseURL);
-  if (params.app) sc2.setApp(params.app);
-  if (params.callbackURL) sc2.setCallbackURL(params.callbackURL);
-  if (params.accessToken) sc2.setAccessToken(params.accessToken);
-  if (params.scope) sc2.setScope(params.scope);
+SteemConnect.prototype.setBaseURL = (baseURL) => {
+  this.options.baseURL = baseURL;
+};
+SteemConnect.prototype.setApp = (app) => {
+  this.options.app = app;
+};
+SteemConnect.prototype.setCallbackURL = (callbackURL) => {
+  this.options.callbackURL = callbackURL;
+};
+SteemConnect.prototype.setAccessToken = (accessToken) => {
+  this.options.accessToken = accessToken;
+};
+SteemConnect.prototype.removeAccessToken = () => {
+  this.options.accessToken = undefined;
+};
+SteemConnect.prototype.setScope = (scope) => {
+  this.options.scope = scope;
 };
 
-sc2.setBaseURL = (baseURL) => { sc2.baseURL = baseURL; };
-sc2.setApp = (app) => { sc2.app = app; };
-sc2.setCallbackURL = (callbackURL) => { sc2.callbackURL = callbackURL; };
-sc2.setAccessToken = (accessToken) => { sc2.accessToken = accessToken; };
-sc2.removeAccessToken = () => { sc2.accessToken = undefined; };
-sc2.setScope = (scope) => { sc2.scope = scope; };
-
-sc2.getLoginURL = (state) => {
-  let loginURL = `${sc2.baseURL}/oauth2/authorize?client_id=${sc2.app}&redirect_uri=${encodeURIComponent(sc2.callbackURL)}`;
-  loginURL += sc2.scope ? `&scope=${sc2.scope.join(',')}` : '';
+SteemConnect.prototype.getLoginURL = (state) => {
+  let loginURL = `${this.baseURL}/oauth2/authorize?client_id=${
+    this.app
+  }&redirect_uri=${encodeURIComponent(this.callbackURL)}`;
+  loginURL += this.scope ? `&scope=${this.scope.join(',')}` : '';
   loginURL += state ? `&state=${encodeURIComponent(state)}` : '';
   return loginURL;
 };
 
-sc2.send = (route, method, body, cb) => {
-  const url = `${sc2.baseURL}/api/${route}`;
+SteemConnect.prototype.send = (route, method, body, cb) => {
+  const url = `${this.baseURL}/api/${route}`;
   const retP = fetch(url, {
     method,
     headers: {
       Accept: 'application/json, text/plain, */*',
       'Content-Type': 'application/json',
-      Authorization: sc2.accessToken,
+      Authorization: this.accessToken,
     },
     body: JSON.stringify(body),
   }).then((res) => {
@@ -58,7 +66,9 @@ sc2.send = (route, method, body, cb) => {
     // If the status is something other than 200 we need
     // to reject the result since the request is not considered as a fail
     if (res.status !== 200) {
-      return Promise.resolve(result).then(result2 => Promise.reject(new SDKError('sc2-sdk error', result2)));
+      return Promise.resolve(result).then(result2 =>
+        Promise.reject(new SDKError('sc2-sdk error', result2))
+      );
     } else if (result.error) {
       return Promise.reject(new SDKError('sc2-sdk error', result));
     }
@@ -67,29 +77,43 @@ sc2.send = (route, method, body, cb) => {
 
   if (!cb) return retP;
 
-  return retP.then((ret) => {
-    if (ret.error) {
-      cb(new SDKError('sc2-sdk error', ret), null);
-    } else {
-      cb(null, ret);
-    }
-  }, err => cb(new SDKError('sc2-sdk error', err), null));
+  return retP.then(
+    (ret) => {
+      if (ret.error) {
+        cb(new SDKError('sc2-sdk error', ret), null);
+      } else {
+        cb(null, ret);
+      }
+    },
+    err => cb(new SDKError('sc2-sdk error', err), null)
+  );
 };
 
-sc2.broadcast = (operations, cb) => sc2.send('broadcast', 'POST', { operations }, cb);
-sc2.me = cb => sc2.send('me', 'POST', {}, cb);
+SteemConnect.prototype.broadcast = (operations, cb) =>
+  this.send('broadcast', 'POST', { operations }, cb);
 
-sc2.vote = (voter, author, permlink, weight, cb) => {
+SteemConnect.prototype.me = cb => this.send('me', 'POST', {}, cb);
+
+SteemConnect.prototype.vote = (voter, author, permlink, weight, cb) => {
   const params = {
     voter,
     author,
     permlink,
     weight,
   };
-  return sc2.broadcast([['vote', params]], cb);
+  return this.broadcast([['vote', params]], cb);
 };
 
-sc2.comment = (parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, cb) => {
+SteemConnect.prototype.comment = (
+  parentAuthor,
+  parentPermlink,
+  author,
+  permlink,
+  title,
+  body,
+  jsonMetadata,
+  cb
+) => {
   const params = {
     parent_author: parentAuthor,
     parent_permlink: parentPermlink,
@@ -99,81 +123,105 @@ sc2.comment = (parentAuthor, parentPermlink, author, permlink, title, body, json
     body,
     json_metadata: JSON.stringify(jsonMetadata),
   };
-  return sc2.broadcast([['comment', params]], cb);
+  return this.broadcast([['comment', params]], cb);
 };
 
-sc2.reblog = (account, author, permlink, cb) => {
+SteemConnect.prototype.reblog = (account, author, permlink, cb) => {
   const params = {
     required_auths: [],
     required_posting_auths: [account],
     id: 'follow',
     json: JSON.stringify([
-      'reblog', {
+      'reblog',
+      {
         account,
         author,
         permlink,
-      }]),
+      },
+    ]),
   };
-  return sc2.broadcast([['custom_json', params]], cb);
+  return this.broadcast([['custom_json', params]], cb);
 };
 
-sc2.follow = (follower, following, cb) => {
+SteemConnect.prototype.follow = (follower, following, cb) => {
   const params = {
     required_auths: [],
     required_posting_auths: [follower],
     id: 'follow',
     json: JSON.stringify(['follow', { follower, following, what: ['blog'] }]),
   };
-  return sc2.broadcast([['custom_json', params]], cb);
+  return this.broadcast([['custom_json', params]], cb);
 };
 
-sc2.unfollow = (unfollower, unfollowing, cb) => {
+SteemConnect.prototype.unfollow = (unfollower, unfollowing, cb) => {
   const params = {
     required_auths: [],
     required_posting_auths: [unfollower],
     id: 'follow',
     json: JSON.stringify(['follow', { follower: unfollower, following: unfollowing, what: [] }]),
   };
-  return sc2.broadcast([['custom_json', params]], cb);
+  return this.broadcast([['custom_json', params]], cb);
 };
 
-sc2.ignore = (follower, following, cb) => {
+SteemConnect.prototype.ignore = (follower, following, cb) => {
   const params = {
     required_auths: [],
     required_posting_auths: [follower],
     id: 'follow',
     json: JSON.stringify(['follow', { follower, following, what: ['ignore'] }]),
   };
-  return sc2.broadcast([['custom_json', params]], cb);
+  return this.broadcast([['custom_json', params]], cb);
 };
 
-sc2.claimRewardBalance = (account, rewardSteem, rewardSbd, rewardVests, cb) => {
+SteemConnect.prototype.claimRewardBalance = (account, rewardSteem, rewardSbd, rewardVests, cb) => {
   const params = {
     account,
     reward_steem: rewardSteem,
     reward_sbd: rewardSbd,
     reward_vests: rewardVests,
   };
-  return sc2.broadcast([['claim_reward_balance', params]], cb);
+  return this.broadcast([['claim_reward_balance', params]], cb);
 };
 
-sc2.revokeToken = cb => sc2.send('oauth2/token/revoke', 'POST', { token: sc2.accessToken }, cb).then(() => sc2.removeAccessToken());
+SteemConnect.prototype.revokeToken = cb =>
+  this.send('oauth2/token/revoke', 'POST', { token: this.accessToken }, cb).then(() =>
+    this.removeAccessToken()
+  );
 
-sc2.updateUserMetadata = (metadata = {}, cb) => sc2.send('me', 'PUT', { user_metadata: metadata }, cb);
+SteemConnect.prototype.updateUserMetadata = (metadata = {}, cb) =>
+  this.send('me', 'PUT', { user_metadata: metadata }, cb);
 
-sc2.sign = (name, params, redirectUri) => {
+SteemConnect.prototype.sign = (name, params, redirectUri) => {
   if (typeof name !== 'string' || typeof params !== 'object') {
     return new SDKError('sc2-sdk error', {
       error: 'invalid_request',
       error_description: 'Request has an invalid format',
     });
   }
-  let url = `${sc2.baseURL}/sign/${name}?`;
-  url += Object.keys(params).map(key =>
-    `${key}=${encodeURIComponent(params[key])}`
-  ).join('&');
+  let url = `${this.baseURL}/sign/${name}?`;
+  url += Object.keys(params)
+    .map(key => `${key}=${encodeURIComponent(params[key])}`)
+    .join('&');
   url += redirectUri ? `&redirect_uri=${encodeURIComponent(redirectUri)}` : '';
   return url;
 };
 
-module.exports = sc2;
+exports.Initialize = (config) => {
+  const instance = new SteemConnect();
+
+  if (!config) {
+    throw new Error('You have to provide config');
+  }
+
+  if (typeof config !== 'object') {
+    throw new Error('Config must be an object');
+  }
+
+  if (config.baseURL) instance.setBaseURL(config.baseURL);
+  if (config.app) instance.setApp(config.app);
+  if (config.callbackURL) instance.setCallbackURL(config.callbackURL);
+  if (config.accessToken) instance.setAccessToken(config.accessToken);
+  if (config.scope) instance.setScope(config.scope);
+
+  return instance;
+};
